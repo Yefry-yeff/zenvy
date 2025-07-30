bc3279ad7b7c6afb75f2038a1e7c56300bc42974
yefry-ortiz-sgsas <yefryyo@gmail.com>
2025-07-30 13:41:41 -0600

    sd

--- Punto_Venta/resources/views/layouts/guest.blade.php
+++ Punto_Venta/resources/views/layouts/guest.blade.php
@@ -14,8 +14,8 @@
     <link href="https://fonts.bunny.net/css?family=figtree:400,500,600&display=swap" rel="stylesheet" />
 
     <!-- Vite -->
-    <link rel="stylesheet" href="{{ asset('build/assets/app-BOca0FAR.css') }}">
-<script type="module" src="{{ asset('build/assets/app-BLl8G-P3.js') }}"></script>
+    <link rel="stylesheet" href="{{ asset('build/assets/app-xQqBX6JP.css') }}">
+<script type="module" src="{{ asset('build/assets/app-Ck2gzFIp.js') }}"></script>
 </head>
 <body class="font-sans antialiased bg-gray-100">
     {{ $slot }}
