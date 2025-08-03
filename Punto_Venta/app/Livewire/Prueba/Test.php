470de2ef41263f0e41fd63fe5e800d61a53258d7
yefry-ortiz-sgsas <yefryyo@gmail.com>
2025-08-02 22:46:47 -0600

    es todo por hoy

---
+++ Punto_Venta/app/Livewire/Prueba/Test.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Livewire\Prueba;
+
+use Livewire\Component;
+
+class Test extends Component
+{
+    public function render()
+    {
+        return view('livewire.prueba.test');
+    }
+}
