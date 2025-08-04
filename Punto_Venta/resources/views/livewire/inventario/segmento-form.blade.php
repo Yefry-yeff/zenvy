04589c2add530a268b128984d754d986f1ab4f6f
Jsruiz97 <167387214+Jsruiz97@users.noreply.github.com>
2025-08-03 21:08:21 -0600

    Arreglo del icono de Segmento

--- Punto_Venta/resources/views/livewire/inventario/segmento-form.blade.php
+++ Punto_Venta/resources/views/livewire/inventario/segmento-form.blade.php
@@ -144,7 +144,7 @@
                     <button wire:click="cerrarModalExito" 
                             class="inline-flex items-center px-4 py-2 bg-green-600 text-white rounded-md hover:bg-green-700 transition-colors duration-200">
                         <svg class="w-4 h-4 mr-1" fill="currentColor" viewBox="0 0 20 20">
-                            <path fill-rule="evenodd" d="M7.707 3.293a1 1 0 010 1.414L5.414 7H11a7 7 0 717 7v2a1 1 0 11-2 0v-2a5 5 0 00-5-5H5.414l2.293 2.293a1 1 0 11-1.414 1.414l-4-4a1 1 0 010-1.414l4-4a1 1 0 011.414 0z" clip-rule="evenodd"></path>
+                            <path fill-rule="evenodd" d="M7.707 3.293a1 1 0 0 1 0 1.414L5.414 7H11a7 7 0 0 1 7 7v2a1 1 0 1 1-2 0v-2a5 5 0 0 0-5-5H5.414l2.293 2.293a1 1 0 1 1-1.414 1.414l-4-4a1 1 0 0 1 0-1.414l4-4a1 1 0 0 1 1.414 0z" clip-rule="evenodd"></path>
                         </svg>
                         Entendido
                     </button>
