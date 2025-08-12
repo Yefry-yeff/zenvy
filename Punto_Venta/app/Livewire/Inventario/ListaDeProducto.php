09aa34ef3b12c09d470b454029cfd7fb70d5a3d1
Jsruiz97 <167387214+Jsruiz97@users.noreply.github.com>
2025-08-12 13:04:09 -0600

    Lista de Productos

---
+++ Punto_Venta/app/Livewire/Inventario/ListaDeProducto.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Livewire\Inventario;
+
+use Livewire\Component;
+
+class ListaDeProducto extends Component
+{
+    public function render()
+    {
+        return view('livewire.inventario.lista-de-producto');
+    }
+}
