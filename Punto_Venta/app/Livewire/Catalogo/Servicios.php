3fab15fbb03a603382a2b20abefc4d66055f6f11
Jsruiz97 <167387214+Jsruiz97@users.noreply.github.com>
2025-08-18 21:25:44 -0600

    Agrego Servicios

---
+++ Punto_Venta/app/Livewire/Catalogo/Servicios.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Livewire\Catalogo;
+
+use Livewire\Component;
+
+class Servicios extends Component
+{
+    public function render()
+    {
+        return view('livewire.catalogo.servicios');
+    }
+}
